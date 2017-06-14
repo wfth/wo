cf3781ea2fbb34af3b68d9b5d33e303ef748a57f
fomentia <isaac@thewilliams.ws>
2017-06-14 15:10:57 -0400

    Beautify form error messages

--- web/views/error_helpers.ex
+++ web/views/error_helpers.ex
@@ -10,7 +10,7 @@ defmodule Wo.ErrorHelpers do
   """
   def error_tag(form, field) do
     if error = form.errors[field] do
-      content_tag :span, translate_error(error), class: "help-block"
+      content_tag :span, translate_error(error), style: "color:red;"
     end
   end
 
