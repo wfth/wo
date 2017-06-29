f2eb24f91658e79af2bc3ca4057c687e9442d381
ijwilliams <isaac@thewilliams.ws>
2017-06-29 17:09:31 -0400

    Rename Search Controller variable to better convey reality

--- web/controllers/search_controller.ex
+++ web/controllers/search_controller.ex
@@ -2,18 +2,18 @@ defmodule Wo.SearchController do
   use Wo.Web, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html", sermons: [], ranks: [])
+    render(conn, "index.html", results: [])
   end
 
   def search(conn, %{"search" => %{"search_term" => search_term}}) do
-    {:ok, sermon_results} = sql("select document_table,
+    {:ok, search_results} = sql("select document_table,
                                         document_id,
                                         ts_rank(content, keywords) as rank
                                         from search_documents, plainto_tsquery('english', $1) keywords
                                         where content @@ keywords", [search_term])
 
-    ranks = Enum.reduce(sermon_results.rows, [], fn(r, ranks) -> ranks ++ [Enum.at(r, 2)] end)
-    sermons = Enum.reduce(sermon_results.rows, [], fn(r, sermons) ->
+    ranks = Enum.reduce(search_results.rows, [], fn(r, ranks) -> ranks ++ [Enum.at(r, 2)] end)
+    results = Enum.reduce(search_results.rows, [], fn(r, results) ->
       {:ok, values} = sql("select ts_headline(title, to_tsquery($1)) as title,
                                   ts_headline(description, to_tsquery($1)) as description,
                                   ts_headline(passages, to_tsquery($1)) as passage"
@@ -21,19 +21,19 @@ defmodule Wo.SearchController do
                                   <> Enum.at(r, 0)
                                   <> " where id = $2",
                                   [search_term, Enum.at(r, 1)])
-      sermons ++ [values]
+      results ++ [values]
     end)
 
-    ranked_sermons_list = ranks
+    ranked_results_list = ranks
                           |> Enum.with_index
                           |> Enum.reduce([], fn({rank, i}, acc) ->
-                               acc ++ [{rank, Enum.at(sermons, i)}]
+                               acc ++ [{rank, Enum.at(results, i)}]
                              end)
-                          |> Enum.sort(fn({rank1, _sermon1}, {rank2, _sermon2}) ->
+                          |> Enum.sort(fn({rank1, _result1}, {rank2, _result2}) ->
                                rank1 >= rank2
                              end)
 
-    render(conn, "index.html", sermons: ranked_sermons_list)
+    render(conn, "index.html", results: ranked_results_list)
   end
 
   defp sql(raw, params) do
