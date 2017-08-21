a3941eb50fc6c9e50d4c5c0ad4c085e9e85c5867
ijwilliams <isaac@thewilliams.ws>
2017-08-21 08:50:23 -0400

    Update quantity when item already in cart

--- lib/wo_web/controllers/visitor/cart_item_controller.ex
+++ lib/wo_web/controllers/visitor/cart_item_controller.ex
@@ -6,9 +6,25 @@ defmodule WoWeb.Visitor.CartItemController do
 
   def create(conn, cart_item_params) do
     {:ok, cart} = unless WoWeb.Session.cart(conn), do: Carts.create_cart(), else: {:ok, WoWeb.Session.cart(conn)}
+    cart = cart |> Wo.Repo.preload(:cart_items)
     conn = WoWeb.Session.put_cart(conn, cart)
 
-    case Carts.create_cart_item(cart, cart_item_params |> Map.put("quantity", 1)) do
+    if cart_item = Enum.find(cart.cart_items, fn(ci) -> ci.resource_id == cart_item_params["resource_id"] |> String.to_integer end) do
+      redirect_with_db_operation(conn, &Carts.update_cart_item/2, cart_item, %{"quantity" => cart_item.quantity + 1})
+    else
+      redirect_with_db_operation(conn, &Carts.create_cart_item/2, cart, cart_item_params |> Map.put("quantity", 1))
+    end
+  end
+
+  def delete(conn, %{"cart_item_id" => cart_item_id}) do
+    cart_item = Carts.get_cart_item!(cart_item_id)
+    Carts.delete_cart_item(cart_item)
+
+    redirect(conn, to: cart_path(conn, :show))
+  end
+
+  defp redirect_with_db_operation(conn, operation, model, params) do
+    case operation.(model, params) do
       {:ok, _cart_item} ->
         conn
         |> put_flash(:info, "Added item to your cart.")
@@ -18,12 +34,5 @@ defmodule WoWeb.Visitor.CartItemController do
         |> put_flash(:error, "Could not add that item to your cart.")
         |> redirect(to: sermon_series_path(conn, :index))
     end
-  end
-
-  def delete(conn, %{"cart_item_id" => cart_item_id}) do
-    cart_item = Carts.get_cart_item!(cart_item_id)
-    Carts.delete_cart_item(cart_item)
-
-    redirect(conn, to: cart_path(conn, :show))
   end
 end
